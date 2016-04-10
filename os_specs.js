43071d53b8b3471874d06872df8d70aca1ab6481
Roshan Ravi <roshanravi10@gmail.com>
2016-04-10 10:51:13 -0400

    Initial Commit

---
+++ os_specs.js
@@ -0,0 +1,18 @@
+var os = require(‘os’);
+
+Module.exports = function() {
+    return {
+        EOL: os.EOL,
+        arch: os.arch(),
+        cpus: os.cpus(),
+        endianness: os.endianness(),
+        homedir: os.homedir(),
+        hostname: os.hostname(),
+        networkInterfaces: os.networkInterfaces(),
+        platform: os.platform(),
+        release: os.release(),
+        tmpdir: os.tmpdir(),
+        totalmem: os.totalmem(),
+        type: os.type()
+    };
+};
